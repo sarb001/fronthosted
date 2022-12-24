bae2724e141940d692b1bfa157ff332589e0ffab
SARB <mrsinghbusiness05@gmail.com>
2022-12-25 05:19:44 +0530

    Working on Port Side

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Post.css';
+
+const Post = () => {
+  return (
+    <div> 
+          <div className="main-container">
+               <div className="main-image"> 
+                <img src = "" alt = "main-image" />
+               </div>
+               <div className="main-title">
+                 <h2> OK let's start Test </h2>
+                </div>
+               <div className="main-date"> 
+                <h3> Date is - </h3>
+                </div>
+               <div className="main-desc">  </div>
+          </div>
+    </div>
+  )
+}
+
+export default Post
