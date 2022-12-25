8d8940bb2e5f1384343df104ed7bf1a55f149ff2
SARB <mrsinghbusiness05@gmail.com>
2022-12-25 08:36:59 +0530

    Singlepost Done

--- src/Components/Singlepost/SinglePost.jsx
+++ src/Components/Singlepost/SinglePost.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Singlepost.css';
+import  {FaEdit} from 'react-icons/fa';
+import {AiFillDelete} from 'react-icons/ai';
 
 const SinglePost = () => {
   return (
@@ -10,19 +12,16 @@ const SinglePost = () => {
                     </div>
               </div>
              <h2 className = 'singlepost-title'> SinglePost   </h2> 
-             <div className="singlepost-icons">
-                 <span> Edit </span>
-                 <span> Delete  </span>
+             <div className = "singlepost-icons" style = {{display:'grid',gridTemplateColumns:'200px 300px',fontSize:'25px',cursor:'pointer'}}>
+                 <span>  <FaEdit /> </span>
+                 <span>  <AiFillDelete />  </span>
              </div>
-             <div className="singlepost-info">
+             <div className = "singlepost-info" style = {{display:'grid',gridTemplateColumns:'1fr 1fr'}}>
                 <span className = 'singlepost-author'> Author: <b> Safak </b> </span>
                 <span className = 'singlepost-date'> 1 hour ago  </span>
              </div>
-             <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam possimus repudiandae 
-              autem maiores deserunt ratione asperiores, error vero, cumque dicta enim illum consectetu
-              r incidunt mollitia exercitationem eligendi omnis explicabo excepturi 
-              deleniti odit ut obcaecati dolores ullam sint. Alias architecto sapiente optio, 
-              totam aliquid tempore nostrum. 
+             <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ipsam possimus repudiandae  autem maiores deserunt ratione asperiores,
               </p>
     </div>
   )
